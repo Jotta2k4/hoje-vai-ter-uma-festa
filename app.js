7f26f8c61c6b4669897ba65b73c219bde09b841a
Jotta2k4 <joao80615@gmail.com>
2025-03-25 23:46:19 -0300

    DOM pronto

---
+++ app.js
@@ -0,0 +1,20 @@
+const botao = document.getElementsByClassName('fogos');
+const conteudo = document.getElementsByClassName('conteudo');
+const texto1 = document.getElementsByClassName('texto-1');
+const texto2 = document.getElementsByClassName('texto-2');
+
+function fogos(){
+    botao[0].style.visibility = 'hidden';
+    conteudo[0].style.visibility = 'visible';
+    texto1[0].style.visibility = 'visible';
+}
+
+function mostraTexto1(){
+    texto1[0].style.visibility = 'visible';
+    texto2[0].style.visibility = 'hidden';
+}
+
+function mostraTexto2(){
+    texto1[0].style.visibility = 'hidden';
+    texto2[0].style.visibility = 'visible';
+}
